de6e610debfa371bd6b120549c564f5dc67d0e5f
Zach Martin <zmironman@gmail.com>
2020-06-24 13:38:46 -0500

    Create tables sql statement

---
+++ SQL scripts/SloshBotTableBuilder.sql
@@ -0,0 +1,152 @@
+create table IngredientType
+(
+	id int not null auto_increment primary key,
+	name nvarchar(100) not null,
+	createdBy nvarchar(100) not null,
+	createdDate datetime not null,
+	modifiedBy nvarchar(100) null,
+	modifiedDate datetime null
+);
+
+create table Ingredient
+(
+	id int not null auto_increment primary key,
+	name nvarchar(100) not null,
+	ingredientTypeID int not null,
+	constraint `Ingredient_fk_ingredient_type`
+        foreign key (ingredientTypeID) references IngredientType(id)
+        on delete restrict
+        on update cascade,
+	alcoholContent int not null,
+	price int,
+	createdBy nvarchar(100) not null,
+	createdDate datetime not null,
+	modifiedBy nvarchar(100) null,
+	modifiedDate datetime null
+);
+
+create table DrinkType(
+    id int not null auto_increment primary key,
+    name nvarchar(100) not null,
+    createdBy nvarchar(100) not null,
+    createdDate datetime not null,
+	modifiedBy nvarchar(100) null,
+	modifiedDate datetime null
+);
+
+create table DefaultIngredients(
+    drinkTypeID int not null,
+    constraint `DefaultIngredients_fk_drink_type`
+        foreign key (drinkTypeID) references DrinkType(id)
+        on delete restrict
+        on update cascade,
+    IngredientTypeID int not null,
+    constraint `DefaultIngredients_fk_ingredient_type`
+        foreign key (ingredientTypeID) references IngredientType(id)
+        on delete restrict
+        on update cascade,
+    amount int not null,
+    createdBy nvarchar(100) not null,
+    createdDate datetime not null,
+	modifiedBy nvarchar(100) null,
+	modifiedDate datetime null
+);
+
+create table Recipe(
+    id int not null auto_increment primary key,
+    name nvarchar(100) not null,
+    drinkTypeID int not null,
+    constraint `Recipe_fk_drink_type`
+        foreign key (drinkTypeID) references DrinkType(id)
+        on delete restrict
+        on update cascade,
+    featured bool,
+    createdBy nvarchar(100) not null,
+    createdDate datetime not null,
+	modifiedBy nvarchar(100) null,
+	modifiedDate datetime null
+);
+
+create table RecipeIngredients(
+    recipeID int not null,
+    constraint `RecipeIngredients_fk_recipe_id`
+        foreign key (recipeID) references Recipe(id)
+        on delete restrict
+        on update cascade,
+    IngredientID int not null,
+    constraint `RecipeIngredients_fk_ingredient_id`
+        foreign key (ingredientID) references Ingredient(id)
+        on delete restrict
+        on update cascade,
+    amount int not null,
+    createdBy nvarchar(100) not null,
+    createdDate datetime not null,
+	modifiedBy nvarchar(100) null,
+	modifiedDate datetime null
+);
+
+create table User(
+    id int not null auto_increment primary key,
+    name nvarchar(100) not null,
+    username nvarchar(100) not null,
+    password nvarchar(100) not null,
+    createdBy nvarchar(100) not null,
+    createdDate datetime not null,
+	modifiedBy nvarchar(100) null,
+	modifiedDate datetime null
+);
+
+create table SloshBot(
+    id int not null auto_increment primary key,
+    name nvarchar(100) not null,
+    ownerID int not null,
+    constraint `SloshBot_fk_owner_id`
+        foreign key (ownerID) references User(id)
+        on delete restrict
+        on update cascade,
+    createdBy nvarchar(100) not null,
+    createdDate datetime not null,
+	modifiedBy nvarchar(100) null,
+	modifiedDate datetime null
+);
+
+create table Optic(
+    id int not null auto_increment primary key,
+    ingredientID int not null,
+    constraint `Optic_fk_ingredient_id`
+        foreign key (ingredientID) references Ingredient(id)
+        on delete restrict
+        on update cascade,
+    distanceFromHome int not null,
+    remainingLiquid int not null,
+    pinNumber int not null,
+    sloshbotID int not null,
+    constraint `Optic_fk_sloshbot_id`
+        foreign key (sloshbotID) references SloshBot(id)
+        on delete restrict
+        on update cascade,
+    createdBy nvarchar(100) not null,
+    createdDate datetime not null,
+	modifiedBy nvarchar(100) null,
+	modifiedDate datetime null
+);
+
+create table OrderHistory(
+    id int not null auto_increment primary key,
+    recipeID int not null,
+    constraint `OrderHistory_fk_recipe_id`
+        foreign key (recipeID) references Recipe(id)
+        on delete restrict
+        on update cascade,
+    userID int not null,
+    constraint `OrderHistory_fk_user_id`
+        foreign key (userID) references User(id)
+        on delete restrict
+        on update cascade,
+    sloshbotID int not null,
+    constraint `OrderHistory_fk_sloshbot_id`
+        foreign key (sloshbotID) references SloshBot(id)
+        on delete restrict
+        on update cascade,
+    createdDate datetime not null
+);
