b118bb67a840412364e08b8e93939d2eab3e9177
Zach Martin <zmironman@gmail.com>
2020-06-24 11:00:59 -0500

    Initial Commit

---
+++ RaspberryPiAPI/src/main/java/sloshbot/raspberrypi_api/RaspberrypiApiApplication.java
@@ -0,0 +1,13 @@
+package sloshbot.raspberrypi_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberrypiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaspberrypiApiApplication.class, args);
+    }
+
+}
---
+++ RaspberryPiAPI/src/test/java/sloshbot/raspberrypi_api/RaspberrypiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sloshbot.raspberrypi_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspberrypiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
