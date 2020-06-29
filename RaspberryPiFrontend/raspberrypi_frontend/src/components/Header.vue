f9192a0d6d8ac96420974eccf0fe80d5a9dd70b5
Zach Martin <zmironman@gmail.com>
2020-06-28 22:00:29 -0500

    Added some dependencies

---
+++ RaspberryPiFrontend/raspberrypi_frontend/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="headerContainer">
+        <div class="navBarContent">
+            <b-navbar toggleable="lg" type="dark" variant="dark">
+                
+            </b-navbar>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- RaspberryPiFrontend/raspberrypi_frontend/src/main.js
+++ RaspberryPiFrontend/raspberrypi_frontend/src/main.js
@@ -4,11 +4,16 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { BootstrapVue, IconsPlugin} from "bootstrap-vue";
+import { LayoutPlugin } from 'bootstrap-vue';
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(LayoutPlugin);
 
 Vue.config.productionTip = false
 
